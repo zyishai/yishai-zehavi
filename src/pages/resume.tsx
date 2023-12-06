3f9e2b170f8823518adbb691bed1b952b3f8c0f7
Yishai Zehavi <zehaviyishai@gmail.com>
2023-12-06 07:46:12 +0200

    uploaded my resume

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -17,27 +17,30 @@ const ResumePage: React.FC<PageProps> = () => {
           <Button
             variant="outline"
             className="flex items-center gap-x-1 bg-slate-50 border-slate-300"
+            asChild
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              strokeWidth="2"
-              stroke="currentColor"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
-              <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
-              <path d="M12 17v-6"></path>
-              <path d="M9.5 14.5l2.5 2.5l2.5 -2.5"></path>
-            </svg>
-            <span className="text-sm text-slate-700 font-medium">
-              Download CV
-            </span>
+            <a href="/resume.pdf" download>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
+                <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
+                <path d="M12 17v-6"></path>
+                <path d="M9.5 14.5l2.5 2.5l2.5 -2.5"></path>
+              </svg>
+              <span className="text-sm text-slate-700 font-medium">
+                Download CV
+              </span>
+            </a>
           </Button>
         </div>
         <Paragraph>
